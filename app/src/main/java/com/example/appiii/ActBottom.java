31b5b3abf495a45e704d496ea2f79d77fb8ef7c4
lchuang8211 <64480625+lchuang8211@users.noreply.github.com>
2020-05-15 01:04:28 +0800

    try bottom nav menu

--- app/src/main/java/com/example/appiii/ActBottom.java
+++ app/src/main/java/com/example/appiii/ActBottom.java
@@ -1,9 +1,12 @@
 package com.example.appiii;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,6 +14,32 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class ActBottom extends AppCompatActivity {
+
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.navigation_home:
+//
+//                    mTextMessage.setText(R.string.title_home);
+//                    showNav(R.id.navigation_home);
+//                    return true;
+//                case R.id.navigation_dashboard:
+//                    mTextMessage.setText(R.string.title_dashboard);
+//                    showNav(R.id.navigation_dashboard);
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    showNav(R.id.navigation_notifications);
+//                    return true;
+//            }
+//            return false;
+//        }
+//    };
+
+    private void showNav(int navigation_ID) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +49,21 @@ public class ActBottom extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_gps, R.id.navigation_dbconnect)
                 .build();
+        //  navigation_dashboard
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        InitialComponent();
+
     }
+
+    private void InitialComponent() {
+//        mTextMessage = findViewById(R.id.message);
+    }
+
+    TextView mTextMessage;
 
 }
